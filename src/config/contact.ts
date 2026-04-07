8207ca9e6cf06984a44a33cc946aea1e5a15d0a3
Jair Gabriel <jairdj14@gmail.com>
2026-04-06 19:27:54 -0600

    update contact.ts

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -1,11 +1,4 @@
-const baseMessage =
-  "Hola, vengo de tu página web y quiero instalar el cotizador de WhatsApp.";
-
 export const contact = {
-  phone: "5216241002876", // 👈 TU NÚMERO
-
-  // 👉 AQUÍ EDITAS EL MENSAJE
-  whatsappLink: `https://wa.me/5216241002876?text=${encodeURIComponent(
-    baseMessage,
-  )}`,
+  whatsappLink:
+    "https://wa.me/5212212239301?text=Hola%2C%20quiero%20probar%20el%20cotizador%20para%20generar%20cotizaciones%20en%20WhatsApp",
 };
